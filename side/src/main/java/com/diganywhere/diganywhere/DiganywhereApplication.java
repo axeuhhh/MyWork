f74c4239150248cdad881570c51860df92fa88eb
axeuhhh <apndombasi@dons.usfca.edu>
2024-11-10 20:57:55 -0800

    side project

---
+++ side/src/main/java/com/diganywhere/diganywhere/DiganywhereApplication.java
@@ -0,0 +1,13 @@
+package com.diganywhere.diganywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiganywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiganywhereApplication.class, args);
+	}
+
+}
